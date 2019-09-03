5d7e857eed3f527491165c714b314b08d736cd6b
Andrej Bauer <Andrej.Bauer@andrej.com>
2019-09-03 11:58:22 +0200

    Comment instructions.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ echo "Deploying math.andrej.com."
 git checkout master
 git pull
 bundle exec jekyll build
+echo "Synching with the server."
 rsync --recursive --links --delete _site/ andrej@lisa.andrej.com:/var/www/mathematics-and-computation/
